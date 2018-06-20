44641473ef41bef7056cf37ec5723fc1756b4b4c
Grzegorz2047 <grzegorz2047@gmail.com>
2018-06-20 17:10:03 +0200

    d

--- loggingin.php
+++ loggingin.php
@@ -24,7 +24,7 @@
 	require_once('functions.php');
 	echo "<a href='index.php' class='btn btn-primary right'>Strona główna</a>";
 	if(isset($_POST['inputUsername']) && isset($_POST['inputPassword'])) {
-		$token = $_COOKIE['MYSID'];
+		//$token = $_COOKIE['MYSID'];
 		$authorized = authorize($_POST['inputUsername'], $_POST['inputPassword'], token);
 		if($authorized) {
 			$_COOKIE['username'] = $_POST['inputUsername'];
