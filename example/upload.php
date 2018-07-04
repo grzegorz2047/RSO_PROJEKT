655afbc6a8e60678236336dfa75492d73ded39d5
grzegorz2047 <grzegorz2047@gmail.com>
2018-07-04 14:33:00 +0200

    test upload 3

--- example/upload.php
+++ example/upload.php
@@ -1,8 +1,12 @@
 <?php
+	include_once("sqlconnect.php");
+	require_once('functions.php');
+	$user = session_check();
 $target_dir = "/var/www/html/uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . $user['username']."_avatar";
+$extension_purpose = basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$imageFileType = strtolower(pathinfo($extension_purpose,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
@@ -37,6 +41,7 @@ if ($uploadOk == 0) {
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+		header("Location:index.php");
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
